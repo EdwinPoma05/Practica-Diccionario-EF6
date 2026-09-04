cb4a8bef8da8fbaa3eff6be42da773864363fd00
EdwinPoma <pomachicomaedwin@gmail.com>
2026-09-04 01:39:34 -0500

    Insertamos un objeto de la clase Repuesto mediante el metodo Add y SaveChange

--- Diccionario/Program.cs
+++ Diccionario/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Diccionario;
+
+Repuesto respuesto = new Repuesto
+{
+    Codigo="ABC123",
+    Nombre="Llantas",
+    Stock=10,
+    Precio=10.25m,
+    Estado= true
+};
+
+TallerContext contexto = new TallerContext();
+
+contexto.Repuestos.Add(respuesto);
+
+
+contexto.SaveChanges();
+
+
+
+Console.WriteLine("Repuesto agregado correctamente.");
